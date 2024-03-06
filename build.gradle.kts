6b5b5ce76fff3b144515e9e229278beccf094bcb
PapaSmurf2k <rxfar6@gmail.com>
2024-03-06 22:51:45 +0000

    Updated gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.8.10"
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("plugin.serialization") version "1.8.10"
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 
 repositories {
@@ -9,8 +12,21 @@ repositories {
 
 dependencies {
     implementation(group = "org.postgresql", name = "postgresql", version = "42.+")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation(group = "org.http4k", name = "http4k-core", version = "4.40.+")
+    implementation(group = "org.http4k", name = "http4k-server-jetty", version = "4.40.+")
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-serialization-json", version = "1.5.+")
+    implementation(group = "org.jetbrains.kotlinx", name = "kotlinx-datetime", version = "0.4.+")
+    implementation(group = "org.slf4j", name = "slf4j-api", version = "2.0.0-alpha5")
+    runtimeOnly(group = "org.slf4j", name = "slf4j-simple", version = "2.0.0-alpha5")
     testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
 }
 
 tasks.register<Copy>("copyRuntimeDependencies") {
