14758cc16b60e2bfcde7904683aca0cc55894461
rxfa <rafa3rnrn@gmail.com>
2024-04-05 02:17:40 +0100

    refactor

--- src/main/kotlin/pt/isel/ls/api/models/PlayerCreate.kt
+++ src/main/kotlin/pt/isel/ls/api/models/PlayerCreate.kt
@@ -4,4 +4,8 @@ import kotlinx.serialization.Serializable
 import pt.isel.ls.utils.emailIsValid
 
 @Serializable
-data class PlayerCreate(val name: String, val email: String)
+data class PlayerCreate(val name: String, val email: String) {
+    init {
+        require(emailIsValid(email)) { "The given email is not in the right format" }
+    }
+}
