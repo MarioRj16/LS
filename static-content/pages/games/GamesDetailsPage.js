53aca43a5359d6a606da92058b8c9265922b55fb
MarioRj16 <mariorijocarvalho@gmail.com>
2024-04-08 02:55:00 +0000

    All pages have something could add more functionalities

--- static-content/pages/games/GamesDetailsPage.js
+++ static-content/pages/games/GamesDetailsPage.js
@@ -1,5 +1,6 @@
 import {FetchAPI} from "../../utils/FetchAPI";
 import {GetPlayer} from "../../components/players/GetPlayer";
+import {GetGame} from "../../components/games/GetGame";
 
 export async function GamesDetailsPage(state){
     const id = state.params.id;
---
+++ static-content/pages/games/GamesPage.js
@@ -0,0 +1,36 @@
+import { div, h1, a, h2, h3 } from "../../utils/Elements";
+import { FetchAPI } from "../../utils/FetchAPI";
+
+export async function GamesPage(state) {
+
+    const games = await FetchAPI(`/games?${state.query}`);
+
+    const handleGameClick = (gameId) => {
+
+        console.log('Redirecting to game details:', gameId);
+        window.location.href = `/games/${gameId}`;
+    };
+
+    return div(
+        h1({ class: "" }, `Games`),
+        div(
+            { class: "game-list" },
+            games.map(game => (
+                a(
+                    {
+                        href: `#`,
+                        onclick: () => handleGameClick(game.id),
+                        key: game.id
+                    },
+                    div(
+                        { class: "game-item" },
+                        h2({}, `Game ID: ${game.id}`),
+                        h3({}, `Title: ${game.title}`),
+                        h3({}, `Developer: ${game.developer || 'Unknown'}`),
+                        h3({}, `Genres: ${game.genres.join(', ') || 'N/A'}`)
+                    )
+                )
+            ))
+        )
+    );
+}
