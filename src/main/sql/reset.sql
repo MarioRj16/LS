875babbdc0ff8b593adb7a64ed933ffa91cf3823
rxfa <rafa3rnrn@gmail.com>
2024-05-05 05:28:02 +0100

    Closes #82-game-name-search-not-case-sensitive

--- src/main/sql/reset.sql
+++ src/main/sql/reset.sql
@@ -1,16 +1,15 @@
 begin;
 
-delete *
-from players_sessions;
-delete *
-from games_genres;
-delete *
-from gaming_sessions;
-delete *
-from games;
-delete *
-from genres;
-delete *
-from players;
+delete * from players_sessions;
+
+delete * from games_genres;
+
+delete * from gaming_sessions;
+
+delete * from games;
+
+delete * from genres;
+
+delete * from players;
 
 commit;
