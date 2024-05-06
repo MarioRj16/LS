f2faa101391998e90d350fd26b347b762fa55aae
MarioRj16 <mariorijocarvalho@gmail.com>
2024-05-06 23:53:51 +0000

    removed sessionoption return

--- static-content/pages/games/GamesSearchPage.js
+++ static-content/pages/games/GamesSearchPage.js
@@ -1,7 +1,6 @@
 import {div, h1, input, button, label} from "../../utils/Elements.js";
 import { FetchAPI } from "../../utils/FetchAPI.js";
-import { GenresOptions } from "../../components/GenresOptions.js";
-import {GenresOptionsInputs} from "../../components/GenresOptionsInputs.js";
+import { GenresOptions, GenresOptionsInputs } from "../../components/GenresOptions.js";
 
 export async function GamesSearchPage(state) {
     const genres = (await FetchAPI(`/genres`)).genres;
